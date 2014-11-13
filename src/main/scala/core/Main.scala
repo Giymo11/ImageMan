b5a4716f359c7e50c45a4d411987974e39bf67bb
Giymo11 <gizmo.head@yahoo.de>
2014-11-14 00:22:08 +0100

    Added the possibility to rename whole folders to include their resolution

--- src/main/scala/core/Main.scala
+++ src/main/scala/core/Main.scala
@@ -4,32 +4,89 @@ import java.io.File
 import javax.imageio.ImageIO
 
 /**
- * Created by Gizmo on 2014-11-13.
+ * Created by Giymo11 on 2014-11-13 at 14:32.
  */
 object Main {
 
-  def main(args: Array[String]) {
-    /*
-    if (args.size != 1)
-      help
-      val file = new File(args(0))
-    */
+  private val COMMAND_DIRECTORY = "-d"
+  private val COMMAND_RENAME_RESOLUTION = "-n"
 
-    val file = new File("1415309926326.jpg")
+  def main(args: Array[String]) {
+    try {
+      // minimum arguments
+      if (args.length < 2) throw new IllegalAccessException
+
+      if (args(0) == COMMAND_DIRECTORY) {
+
+        if (args.length != 3) throw new IllegalArgumentException
+        val dir = new File(args(2))
+
+        if (!dir.exists() || !dir.isDirectory) throw new IllegalArgumentException
+        dir.listFiles.par.foreach(handle(_, args(1)))
+
+      } else {
+
+        if (args.length != 2) throw new IllegalArgumentException
+        val file = new File(args(1))
+
+        if (!file.exists() || file.isDirectory) throw new IllegalArgumentException
+        handle(file, args(0))
+
+      }
+    } catch {
+
+      case e: Exception => printHelp(e.getMessage)
+
+    }
+
+    args.foreach(println)
+  }
+
+  def handle(file: File, command: String) = {
+    val filename = file.getName
+    assert(file.exists())
+
+    def isPicture: Boolean = file.getName.endsWith("png") || file.getName.endsWith("jpg")
+
+    def isWellNamed: Boolean = {
+      // matches something like [1920x1080]filename.jpg
+      // doesn't match something like [1920x1080][123421]filename.jpg
+      filename.matches("\\[\\d*x\\d*\\][^\\[\\]]*")
+    }
+
+    if (isPicture)
+      if (command == COMMAND_RENAME_RESOLUTION)
+        if (!isWellNamed) // TODO: make this check optional
+          renameWithResolution(file)
+        else
+          println(s"$filename not renamed because already named well")
+      else throw new IllegalArgumentException(command)
+  }
+
+  def renameWithResolution(file: File) = {
+    val filename = file.getName
     val image = ImageIO.read(file)
 
-    val filename = file.getName
-    val newFilename = "(" + image.getWidth + "x" + image.getHeight + ")" + file.getName.split("\\.")(0) + "-processed.png"
+    val fileEnding = filename.split("\\.")(1)
+
+    var fileBeginning = filename.split("\\.")(0)
+
+    val indexOfBracket = fileBeginning.lastIndexOf(']')
+
+    if (indexOfBracket != -1) fileBeginning = fileBeginning.substring(indexOfBracket + 1)
+
+    val newFilename = "[" + image.getWidth + "x" + image.getHeight + s"]$fileBeginning.$fileEnding"
+
+    file.renameTo(new File(file.getParentFile.getPath, newFilename))
     println(s"From $filename to $newFilename")
-    ImageIO.write(image, "png", new File(newFilename))
   }
 
+  def printHelp(message: String) {
+    if (message != null) println("Exception: " + message)
+    println("Usage: [" + COMMAND_DIRECTORY + "] (" + COMMAND_RENAME_RESOLUTION + ") (directoryName|fileName)")
+    println("Be aware that a filename cannot contain more than one .")
+    println(COMMAND_DIRECTORY + " : apply operation to all image-files in the directory")
+    println(COMMAND_RENAME_RESOLUTION + ", --rename-with-resolution : renames the picture to fit its resolution.")
 
-  def help {
-    println("Please provide the arguments: Filename")
-    println("Description: Quantizes the input image and produces the results in out.png")
-    System.exit(1)
   }
-
-
 }
